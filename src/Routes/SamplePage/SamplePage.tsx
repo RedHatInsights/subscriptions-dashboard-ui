c866f5296bd7ace54b23dbbca5799586a876b87a
rsun19 <robertssun1234@gmail.com>
2023-05-22 16:31:47 -0400

    made top card

--- src/Routes/SamplePage/SamplePage.tsx
+++ src/Routes/SamplePage/SamplePage.tsx
@@ -7,6 +7,12 @@ import {
   Stack,
   StackItem,
   Title,
+  Card,
+  CardTitle,
+  CardBody,
+  Divider,
+  Flex,
+  FlexItem
 } from '@patternfly/react-core';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import {
@@ -48,10 +54,18 @@ const SamplePage = () => {
 
   return (
     <React.Fragment>
-      <PageHeader>
-        <PageHeaderTitle title="Sample Insights App" />
-        <p> This is page header text </p>
-      </PageHeader>
+      <Card>
+        <Flex display={{ default: 'inlineFlex' }} style={{ display: 'flex', flexWrap: 'nowrap', alignItems: 'start' }}>
+          <FlexItem style={{ marginTop: '30px', marginLeft: '20px'}}>
+            <img style={{ height: "100%", width:"75px", objectFit: 'cover' }} src= { require('../../../static/images/Product_icon-Red_Hat-Generic-Service-RGB.png') }></img>
+          </FlexItem>
+          <Divider orientation={{ default: 'vertical' }} style={{ marginLeft: '5px' , marginTop: '20px', marginBottom: '20px'}} />
+          <FlexItem style={{ margin: '0px 0 0 0px', height: '150px'}}>
+            <CardBody style={{ fontWeight: "bold", fontSize: "25px" }}>Welcome to Subcription services</CardBody>
+            <CardBody style={{ marginRight: '100px' }}>Empower your buying decisions with data. Subscription services provide reporting that is designed to make your subscription choices data-driven</CardBody>
+          </FlexItem>
+        </Flex>
+      </Card>
       <Main>
         <Stack hasGutter>
           <StackItem>
