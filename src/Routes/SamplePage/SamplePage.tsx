60d04edc83f2e257d00153d5cb5715a52626f22c
rsun19 <robertssun1234@gmail.com>
2023-05-22 17:46:46 -0400

    fixed top card

--- src/Routes/SamplePage/SamplePage.tsx
+++ src/Routes/SamplePage/SamplePage.tsx
@@ -12,7 +12,9 @@ import {
   CardBody,
   Divider,
   Flex,
-  FlexItem
+  FlexItem,
+  List,
+  ListItem
 } from '@patternfly/react-core';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import {
@@ -61,22 +63,43 @@ const SamplePage = () => {
           </FlexItem>
           <Divider orientation={{ default: 'vertical' }} style={{ marginLeft: '5px' , marginTop: '20px', marginBottom: '20px'}} />
           <FlexItem style={{ margin: '0px 0 0 0px', height: '150px'}}>
-            <CardBody style={{ fontWeight: "bold", fontSize: "25px" }}>Welcome to Subcription services</CardBody>
-            <CardBody style={{ marginRight: '100px' }}>Empower your buying decisions with data. Subscription services provide reporting that is designed to make your subscription choices data-driven</CardBody>
+            <CardBody style={{ display: 'flex', flexDirection: 'column' }}>
+                <p style={{ fontWeight: "bold", fontSize: "25px", marginBottom: '10px'}}>Welcome to Subscription services</p>
+                <p style={{ fontSize: '15px' }}>Empower your buying decisions with data. Subscription services provide reporting that is designed to make your subscription choices data-driven</p>
+            </CardBody>
+            {/* <CardBody style={{ marginRight: '100px' }}>Empower your buying decisions with data. Subscription services provide reporting that is designed to make your subscription choices data-driven</CardBody> */}
           </FlexItem>
         </Flex>
       </Card>
       <Main>
         <Stack hasGutter>
           <StackItem>
-            <Title headingLevel="h2" size="3xl">
-              {' '}
-              Alerts{' '}
-            </Title>
-            <Button variant="primary" onClick={handleAlert}>
-              {' '}
-              Dispatch alert{' '}
-            </Button>
+          <Card>
+            <Flex display={{ default: 'inlineFlex' }} style={{ display: 'flex', flexWrap: 'nowrap', alignItems: 'start' }}>
+              <FlexItem flex={{ default: 'flex_3' }}>
+                <CardBody style={{ fontWeight: "bold", fontSize: "25px" }}>See all your subscriptions</CardBody>
+                <CardBody style={{ fontWeight: "bold", fontSize: "15px" }}>Register your RHEL systems</CardBody>
+                <CardBody style={{ fontSize: "15px" }}>Get the full value of your RHEL subscriptions, including updates and security patches, through registration</CardBody>
+                <List>
+                  <ListItem>For a guided registration experience, try the <AppLink to="/"> Registration Assistant </AppLink>.</ListItem>
+                  <ListItem>Review registration alternatives and select the best option for your workflow. <AppLink to="/"> Learn more </AppLink>.</ListItem>
+                </List>
+                <CardBody style={{ fontWeight: "bold", fontSize: "15px" }}>Activate Subscriptions Usage</CardBody>
+                <CardBody style={{ fontSize: "15px" }}>
+                  Gain visibility into the usage of your subcriptions against your total capacity over time. Opt in to Subcriptions Usage today! 
+                  <AppLink to="/"> Learn more </AppLink>.
+                </CardBody>
+                <CardBody style={{ fontWeight: "bold", fontSize: "15px" }}>Get the right usage data</CardBody>
+                <CardBody style={{ fontSize: "15px" }}>
+                  Account-wide usage reporting relies on multiple data streams to power Subscriptions Usage. Make sure that the right data is flowing for accurate reporting. 
+                  <AppLink to="/"> Learn more </AppLink>.
+                </CardBody>
+              </FlexItem>
+              <FlexItem flex={{ default: 'flex_1' }} style={{ marginTop: '30px', height: "100%,", objectFit: 'cover'}}>
+                <img src= { require('../../../static/images/subscriptionView.png') }></img>
+              </FlexItem>
+            </Flex>
+          </Card>
           </StackItem>
           <StackItem>
             <Suspense fallback={<Spinner />}>
