157863d4fe6281cda1a5a561c6d511579a0f0a22
rsun19 <robertssun1234@gmail.com>
2023-05-25 11:33:13 -0400

    added subscription links

--- src/Routes/SamplePage/SubscriptionPortfolio.tsx
+++ src/Routes/SamplePage/SubscriptionPortfolio.tsx
@@ -12,50 +12,46 @@ import { redirect } from 'react-router-dom';
 
 const SubscriptionPortfolio = () => {
 
-    const handleClick = (num: number) => {
-        if (num == 1) {
-            return redirect("/")
-        } else if (num == 2) {
-            return redirect("/")
-        } else if (num == 3) {
-            return redirect("/")
-        } else {
-            return redirect("/")
-        }
-    }
-
     return (
         <Flex display={{ default: 'inlineFlex' }} style={{ display: 'flex', flexWrap: 'nowrap', alignItems: 'start' }}>
             <FlexItem flex={{ default: 'flex_1' }}>
                 <Card style={{ textAlign: 'center' }} className='portfolioFlex'>
-                    <Button onClick={() => handleClick(1)} style={{ background: 'white', padding: 0}}>
-                        <img className='standardImage' src= { require('../../../static/images/rhel.jpg') }></img>
-                        <AppLink to="/">Red Hat<br />Enterprise Linux</AppLink>       
-                    </Button>
+                    <a target="_blank" href="https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux">
+                        <Button style={{ background: 'white', padding: 0}}>
+                            <img className='standardImage' src= { require('../../../static/images/rhel.jpg') }></img>
+                            <a target="_blank" href="https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux">Red Hat<br />Enterprise Linux</a>       
+                        </Button>
+                    </a>
                 </Card>
             </FlexItem>
             <FlexItem flex={{ default: 'flex_1' }}>
                 <Card style={{ textAlign: 'center' }} className='portfolioFlex'>
-                    <Button onClick={() => handleClick(2)} style={{ background: 'white', padding: 0}}>
-                        <img className='standardImage' src= { require('../../../static/images/openshift.jpg') }></img>
-                        <AppLink to="/">Red Hat<br />OpenShift</AppLink>
-                    </Button>
+                    <a target="_blank" href="hhttps://www.redhat.com/en/technologies/cloud-computing/openshift">
+                        <Button style={{ background: 'white', padding: 0}}>
+                            <img className='standardImage' src= { require('../../../static/images/openshift.jpg') }></img>
+                            <a target="_blank" href="https://www.redhat.com/en/technologies/cloud-computing/openshift">Red Hat<br />OpenShift</a>
+                        </Button>
+                    </a>
                 </Card>
             </FlexItem>
             <FlexItem flex={{ default: 'flex_1' }}>
                 <Card style={{ textAlign: 'center' }} className='portfolioFlex'>
-                    <Button onClick={() => handleClick(3)} style={{ background: 'white', padding: 0}}>
-                        <img className='standardImage' src= { require('../../../static/images/ansible.jpg') }></img>
-                        <AppLink to="/">Red Hat<br />Ansible Automation Platform</AppLink>
-                    </Button>
+                    <a target='_blank' href='https://www.redhat.com/en/technologies/management/ansible'>
+                        <Button style={{ background: 'white', padding: 0}}>
+                            <img className='standardImage' src= { require('../../../static/images/ansible.jpg') }></img>
+                            <a target='_blank' href="https://www.redhat.com/en/technologies/management/ansible">Red Hat<br />Ansible Automation Platform</a>
+                        </Button>
+                    </a>
                 </Card>
             </FlexItem>
             <FlexItem flex={{ default: 'flex_1' }}>
                 <Card style={{ textAlign: 'center' }} className='portfolioFlex'>
-                    <Button onClick={() => handleClick(4)} style={{ background: 'white', padding: 0 }}>
-                        <PlusCircleIcon className='standardImage' style={{ color: 'gray' }}/>
-                        <AppLink to="/">View all<br />Red Hat Products</AppLink>
-                    </Button>
+                    <a target="_blank" href='https://www.redhat.com/en/technologies/all-products'>
+                        <Button style={{ background: 'white', padding: 0 }}>
+                            <PlusCircleIcon className='standardImage' style={{ color: 'gray' }}/>
+                            <a target='_blank' href="https://www.redhat.com/en/technologies/all-products">View all<br />Red Hat Products</a>
+                        </Button>
+                    </a>
                 </Card>
             </FlexItem>
         </Flex>
