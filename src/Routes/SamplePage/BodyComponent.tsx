53f908071e75be6295a787dfad95161e4ce06756
rsun19 <robertssun1234@gmail.com>
2023-05-25 11:21:12 -0400

    added links for body component

--- src/Routes/SamplePage/BodyComponent.tsx
+++ src/Routes/SamplePage/BodyComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect } from 'react';
+import React from 'react';
 import {
   Card,
   CardBody,
@@ -21,22 +21,22 @@ const BodyComponent = () => {
                   <p className='titleCard bottomMargins'>Register your RHEL systems</p>
                   <p className='bodyFont bottomMargins'>Get the full value of your RHEL subscriptions, including updates and security patches, through registration</p>
                   <List>
-                    <ListItem className='bodyFont'>For a guided registration experience, try the <AppLink to="/"> Registration Assistant <ExternalLinkAltIcon /></AppLink></ListItem>
-                    <ListItem className='bodyFont'>Review registration alternatives and select the best option for your workflow. <AppLink to="/"> Learn more <ExternalLinkAltIcon /></AppLink></ListItem>
+                    <ListItem className='bodyFont'>For a guided registration experience, try the <a target = "_blank" href="https://access.redhat.com/labs/registrationassistant/"> Registration Assistant <ExternalLinkAltIcon /></a></ListItem>
+                    <ListItem className='bodyFont'>Review registration alternatives and select the best option for your workflow. <a target="_blank" href="https://access.redhat.com/documentation/en-us/subscription_central/2023/html-single/getting_started_with_rhel_system_registration/index"> Learn more <ExternalLinkAltIcon /></a></ListItem>
                   </List>
                 </CardBody>
                 <CardBody>
                   <p className='titleCard bottomMargins'>Activate Subscription Usage</p>
                   <p className='bodyFont'>
                   Gain visibility into the usage of your subcriptions against your total capacity over time. Opt in to Subcriptions Usage today!
-                  &nbsp;<AppLink to="/">Learn more <ExternalLinkAltIcon /></AppLink>
+                  &nbsp;<a target="_blank" href="https://access.redhat.com/documentation/en-us/subscription_central/2023/html-single/getting_started_with_the_subscriptions_service/index#assembly-activating-opening-subscriptionwatch">Learn more <ExternalLinkAltIcon /></a>
                   </p>
                 </CardBody>
                 <CardBody>
                   <p className='titleCard bottomMargins'>Get the right usage data</p>
                   <p className='bodyFont'>
                   Account-wide usage reporting relies on multiple data streams to power Subscription Usage. Make sure that the right data is flowing for accurate reporting.
-                  &nbsp;<AppLink to="/">Learn more <ExternalLinkAltIcon /></AppLink>
+                  &nbsp;<a target="_blank" href="https://access.redhat.com/documentation/en-us/subscription_central/2023/html-single/getting_started_with_the_subscriptions_service/index#assembly-setting-up-subscriptionwatch">Learn more <ExternalLinkAltIcon /></a>
                   </p>
                 </CardBody>
               </FlexItem>
