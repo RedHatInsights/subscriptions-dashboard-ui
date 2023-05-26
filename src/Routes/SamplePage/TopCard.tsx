983c5a52407c736d9f45405edc5a3743203ab562
rsun19 <robertssun1234@gmail.com>
2023-05-26 10:38:21 -0400

    made title card responsive

--- src/Routes/SamplePage/TopCard.tsx
+++ src/Routes/SamplePage/TopCard.tsx
@@ -12,11 +12,11 @@ const TopCard = () => {
     return (
       <Card>
         <Flex display={{ default: 'inlineFlex' }} style={{ display: 'flex', flexWrap: 'nowrap', alignItems: 'start' }}>
-          <FlexItem style={{ marginTop: '30px', marginLeft: '20px', marginRight: '5px'}}>
+          <FlexItem style={{ marginTop: '30px', marginLeft: '20px', marginRight: '5px', flex: '1'}}>
             <img className='topImage' src= { require('../../../static/images/subs.png') }></img>
           </FlexItem>
           <Divider orientation={{ default: 'vertical' }} style={{ margin: '30px 0px 30px 20px' }} />
-          <FlexItem style={{ margin: '0px 20% 0px 0px', height: '150px'}}>
+          <FlexItem className='titleCardRightFlex' style={{ flex: '8' }}>
             <CardBody>
                 <p className='headingFont bottomMargins'>Welcome to Subscription Services</p>
                 <p className='bodyFont'>Empower your buying decisions with data. Subscription services provide reporting that is designed to make your subscription choices data-driven</p>
